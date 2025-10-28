dd5f52506de22724227f624dc4fba30518bd7b0b
Eric <shantz.eric@gmail.com>
2025-10-28 16:23:31 -0400

    add base path to nextconfig.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
+  basePath: '/ES_Portfolio_2025',
+  assetPrefix: '/ES_Portfolio_2025',
 
   // optional: make builds forgiving during setup
   eslint: { ignoreDuringBuilds: true },
